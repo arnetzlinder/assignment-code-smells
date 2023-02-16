8b54b1927a5ddeb5ce5357867bd46f5f4725e63f
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-02-16 10:25:12 +0100

    sa

---
+++ src/ts/models/Product.ts
@@ -0,0 +1,10 @@
+class Product {
+    constructor(
+        public name: string,
+        public price: number,
+        public amount: number,
+        public description: string,
+        public image: string,
+        public parent: HTMLElement
+    ) {}
+}
