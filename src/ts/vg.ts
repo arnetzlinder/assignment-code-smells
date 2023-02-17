e72202dee1de7fc958afa81841277dc021df5706
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-02-17 16:56:42 +0100

    some last additions

--- src/ts/vg.ts
+++ src/ts/vg.ts
@@ -1,7 +1,7 @@
 /*
 1. Se om du kan hitta problem med koden nedan och se om du kan göra den bättre.
 */
-export enum Sort {
+export enum SortBy {
   PRICE_ASCENDING = "Stigande pris",
   PRICE_DECENDING = "Sjunkande pris",
   NAME_ALPHABETIC = "Alfabetisk ordning",
@@ -10,12 +10,12 @@ export enum Sort {
 
 export class illegalSortSelectedException extends Error {}
 
-export function sortProductsBy (sort: Sort, products: Product[]): Product[] {
-  if (sort === Sort.PRICE_ASCENDING) {
+export function sortProductsBy (sort: SortBy, products: Product[]): Product[] {
+  if (sort === SortBy.PRICE_ASCENDING) {
     return products.sort((a,b) => { return a.price - b.price });
-  } else if (sort === Sort.PRICE_DECENDING) {
+  } else if (sort === SortBy.PRICE_DECENDING) {
     return products.sort((a,b) => { return b.price - a.price });
-  } else if (sort === Sort.NAME_ALPHABETIC) {
+  } else if (sort === SortBy.NAME_ALPHABETIC) {
     return products.sort((a,b) => { 
       if (a.name > b.name)
         return 1;
@@ -23,7 +23,7 @@ export function sortProductsBy (sort: Sort, products: Product[]): Product[] {
         return -1;
       return 0;
     });
-  } else if (sort === Sort.NAME_ALPHABETIC_REVERSE) {
+  } else if (sort === SortBy.NAME_ALPHABETIC_REVERSE) {
     return products.sort((a,b) => { 
       if (a.name < b.name)
         return 1;
@@ -35,241 +35,232 @@ export function sortProductsBy (sort: Sort, products: Product[]): Product[] {
   // Om sort defineras fel kommer en tom lista att returneras. Det undviker vi med att istället skicka tillbaka ett fel. 
   throw new illegalSortSelectedException();
 }
-// export function sortProductsBy(sort: Sort, products: Product[]): Product[] {
-//   let copiedList: Product[] = [];
-//   products.forEach((product) => copiedList.push(product));
-
-//   let sortedList: Product[] = [];
-//   if (sort === Sort.PRICE_ASCENDING) {
-//     sortedList = sortList("Price", copiedList);
-//     sortedList.reverse();
-//   } else if (sort === Sort.PRICE_DECENDING) {
-//     sortedList = sortList("Price", copiedList);
-//   } else if (sort === Sort.NAME_ALPHABETIC) {
-//     sortedList = sortList("Name", copiedList);
-//   } else if (sort === Sort.NAME_ALPHABETIC_REVERSE) {
-//     sortedList = sortList("Name", copiedList);
-//     sortedList.reverse();
-//   }
-
-//   return sortedList;
-// }
-
-// function sortList(whichAttribute: string, products: Product[]): Product[] {
-//   return products.sort((p1, p2) => {
-//     if (whichAttribute === "Price") {
-//       if (p1.price < p2.price) {
-//         return 1;
-//       } else if (p1.price > p2.price) {
-//         return -1;
-//       }
-//       return 0;
-//     } else {
-//       if (p1.name < p2.name) {
-//         return 1;
-//       } else if (p1.name > p2.name) {
-//         return -1;
-//       }
-//       return 0;
-//     }
-//   });
-// }
 
 /*
   2. Refaktorera funktionen createProductHtml :)
   */
-class Cart {
-  addToCart(i: number) {}
-}
-export let cartList = JSON.parse(localStorage.getItem("savedCartList") || "[]");
-export let productList = JSON.parse(localStorage.getItem("savedList") || "[]");
-
-export function createProductHtml() {
-  let quantity = 0;
-  for (let i = 0; i < cartList.length; i++) {
-    quantity += cartList[i].quantity;
+  class Cart {
+    addToCart(i: number) {}
   }
-  let floatingCart = document.getElementById(
-    "floatingcartnumber"
-  ) as HTMLElement;
-  floatingCart.innerHTML = "" + quantity;
+  export let cartList = JSON.parse(localStorage.getItem("savedCartList") || "[]");
+  export let productList = JSON.parse(localStorage.getItem("savedList") || "[]");
 
-  for (let i = 0; i < productList.length; i++) {
-    let dogproduct: HTMLDivElement = document.createElement("div");
-    let dogImgContainer: HTMLDivElement = document.createElement("div");
-    dogImgContainer.className = "dogimgcontainer";
-    dogproduct.appendChild(dogImgContainer);
-    let dogImg: HTMLImageElement = document.createElement("img");
+  function createHtml() {
 
-    dogImg.src = productList[i].picture;
-    dogImg.alt = productList[i].pictureAlt;
+    updateCart();
 
-    dogImg.addEventListener("mouseover", () => {
+    for (let i = 0; i < productList.length; i++) {
+      let dogproduct: HTMLDivElement = document.createElement("div");
+
+      let renderDogImage = createDogImage(i);
+      dogproduct.appendChild(renderDogImage);
+
+      createName(i, dogproduct);
+
+      createPrice(i, dogproduct);
+  
+      createInfo(i, dogproduct);
+
+      sortBy(i, dogproduct);
+    }
+  }
+
+  function createDogImage(i: number) {
+    let dogImageContainer: HTMLDivElement = document.createElement("div");
+    dogImageContainer.className = "dogimgcontainer";
+
+    let dogImage: HTMLImageElement = document.createElement("img");
+
+    dogImage.src = productList[i].picture;
+    dogImage.alt = productList[i].pictureAlt;
+
+    dogImage.addEventListener("mouseover", () => {
       cartSymbolContainer.classList.add("hover");
-      dogImg.classList.add("hover");
     });
 
-    dogImg.addEventListener("mouseout", () => {
-      dogImg.classList.remove("hover");
-      cartSymbolContainer.classList.remove("hover");
-    });
+    dogImageContainer.appendChild(dogImage);
 
-    dogImgContainer.appendChild(dogImg);
+    dogImageContainer.appendChild(dogImage);
     let cartSymbolContainer: HTMLDivElement = document.createElement("div");
     cartSymbolContainer.className = "cartSymbolContainer";
-    dogImgContainer.appendChild(cartSymbolContainer);
+    dogImageContainer.appendChild(cartSymbolContainer);
 
     let cartSymbol: HTMLElement = document.createElement("i");
     cartSymbol.className = "bi bi-bag-plus";
     cartSymbolContainer.appendChild(cartSymbol);
 
-    let name: HTMLHeadingElement = document.createElement("h5");
-    name.innerHTML = productList[i].name;
-    dogproduct.appendChild(name);
-
-    let price: HTMLHeadingElement = document.createElement("p");
-    price.innerHTML = "$" + productList[i].price;
-    dogproduct.appendChild(price);
-
-    let info: HTMLHeadingElement = document.createElement("p");
-    info.innerHTML = productList[i].info;
-    dogproduct.appendChild(info);
+    cartSymbol.addEventListener("click", () => {
+    let cart = new Cart();
+    cart.addToCart(i);
+    });
 
     productList[i].productSpec = false;
 
-    dogImg.addEventListener("click", () => {
+    dogImage.addEventListener("click", () => {
       productList[i].productSpec = !productList[i].productSpec;
       window.location.href = "product-spec.html#backArrow";
-      let listastext = JSON.stringify(productList);
-      localStorage.setItem("savedList", listastext);
-    });
+      let listText = JSON.stringify(productList);
+      localStorage.setItem("savedList", listText);
+      });
 
-    cartSymbol.addEventListener("click", () => {
-      let cart = new Cart();
-      cart.addToCart(i);
-    });
+      return dogImageContainer;
+    }
 
-    if (productList[i].category === "sassy") {
-      let cat1: HTMLElement = document.getElementById("sassy") as HTMLElement;
+  setLocalStorage();
+
+
+  function createName(i: number, dogproduct: HTMLDivElement) {
+    let name: HTMLHeadingElement = document.createElement("h5");
+    name.innerHTML = productList[i].name;
+    dogproduct.appendChild(name);
+  }
+
+  function createPrice(i: number, dogproduct: HTMLElement) {
+    let price: HTMLHeadingElement = document.createElement("p");
+    price.innerHTML = "$" + productList[i].price;
+    dogproduct.appendChild(price);
+  }
+
+  function createInfo(i: number, dogproduct: HTMLElement) {
+    let info: HTMLHeadingElement = document.createElement("p");
+    info.innerHTML = productList[i].info;
+    dogproduct.appendChild(info);
+  }
+
+  function setLocalStorage() {
+    let listastext = JSON.stringify(productList);
+    localStorage.setItem("savedList", listastext);
+    sessionStorage.clear();
+  }
+
+  function updateCart() {
+    let quantity = 0;
+    for (let i = 0; i < cartList.length; i++) {
+      quantity += cartList[i].quantity;
+    }
+  
+    let floatingCart = document.getElementById("floatingcartnumber") as HTMLElement;
+    floatingCart.innerHTML = "" + quantity;
+  }
+  
+  function sortBy(i: number, dogproduct: HTMLDivElement) {
+    if (productList[i].category === "dogcollars") {
+      let cat1: HTMLElement = document.getElementById("dogcollars") as HTMLElement;
       dogproduct.className = "dogproduct";
       cat1.appendChild(dogproduct);
     }
-    if (productList[i].category === "kriminella") {
+    if (productList[i].category === "dogfood") {
       let cat2: HTMLElement = document.getElementById(
-        "kriminella"
+        "dogfood"
       ) as HTMLElement;
       dogproduct.className = "dogproduct";
       cat2.appendChild(dogproduct);
     }
-    if (productList[i].category == "singlar") {
-      let cat3: HTMLElement = document.getElementById("singlar") as HTMLElement;
+    if (productList[i].category == "dogtoys") {
+      let cat3: HTMLElement = document.getElementById("dogtoys") as HTMLElement;
       dogproduct.className = "dogproduct";
       cat3.appendChild(dogproduct);
     }
-    if (productList[i].category === "puppy") {
-      let cat4: HTMLElement = document.getElementById("puppy") as HTMLElement;
+    if (productList[i].category === "poopbags") {
+      let cat4: HTMLElement = document.getElementById("poopbags") as HTMLElement;
       dogproduct.className = "dogproduct";
       cat4.appendChild(dogproduct);
     }
-    if (productList[i].category === "oldies") {
-      let cat5: HTMLElement = document.getElementById("oldies") as HTMLElement;
+    if (productList[i].category === "dogvitamines") {
+      let cat5: HTMLElement = document.getElementById("dogvitamines") as HTMLElement;
       dogproduct.className = "dogproduct";
       cat5.appendChild(dogproduct);
     }
   }
-  let listastext = JSON.stringify(productList);
-  localStorage.setItem("savedList", listastext);
-  sessionStorage.clear();
-}
-
+  
 /*
   3. Refaktorera funktionen getfromstorage
   */
-export class CartProduct {
-  constructor(
-    public name: string,
-    public image: string,
-    public price: number,
-    public amount: number
-  ) {}
-}
+// export class CartProduct {
+//   constructor(
+//     public name: string,
+//     public image: string,
+//     public price: number,
+//     public amount: number
+//   ) {}
+// }
 
-function getfromstorage() {
-  let container = document.getElementById("checkout-table");
+// function getfromstorage() {
+//   let container = document.getElementById("checkout-table");
 
-  let fromstorage: string = localStorage.getItem("cartArray") || "";
-  let astext: CartProduct[] = JSON.parse(fromstorage);
+//   let fromstorage: string = localStorage.getItem("cartArray") || "";
+//   let astext: CartProduct[] = JSON.parse(fromstorage);
 
-  let productcontainer = document.getElementById(
-    "product-ckeckout-container"
-  ) as HTMLDivElement;
+//   let productcontainer = document.getElementById(
+//     "product-ckeckout-container"
+//   ) as HTMLDivElement;
 
-  let amountcontainer = document.getElementById(
-    "amount-checkout-container2"
-  ) as HTMLDivElement;
-  let amounttext: HTMLTableCellElement = document.createElement("th");
-  amountcontainer.appendChild(amounttext);
-  amounttext.innerHTML = "amount:";
+//   let amountcontainer = document.getElementById(
+//     "amount-checkout-container2"
+//   ) as HTMLDivElement;
+//   let amounttext: HTMLTableCellElement = document.createElement("th");
+//   amountcontainer.appendChild(amounttext);
+//   amounttext.innerHTML = "amount:";
 
-  let titlecontainer = document.getElementById(
-    "title-container"
-  ) as HTMLTableRowElement;
-  titlecontainer.innerHTML = "<strong>products:</strong>";
+//   let titlecontainer = document.getElementById(
+//     "title-container"
+//   ) as HTMLTableRowElement;
+//   titlecontainer.innerHTML = "<strong>products:</strong>";
 
-  let productquantity = document.getElementById(
-    "product-quantity"
-  ) as HTMLTableRowElement;
-  let qttext: HTMLTableCellElement = document.createElement("th");
-  productquantity.appendChild(qttext);
-  qttext.innerHTML = "change quantity:";
+//   let productquantity = document.getElementById(
+//     "product-quantity"
+//   ) as HTMLTableRowElement;
+//   let qttext: HTMLTableCellElement = document.createElement("th");
+//   productquantity.appendChild(qttext);
+//   qttext.innerHTML = "change quantity:";
 
-  let checkkouttotal2 = document.getElementById(
-    "title-total"
-  ) as HTMLTableCellElement;
-  let totaltext: HTMLTableCellElement = document.createElement("th");
-  checkkouttotal2.appendChild(totaltext);
-  totaltext.innerHTML = "total:";
+//   let checkkouttotal2 = document.getElementById(
+//     "title-total"
+//   ) as HTMLTableCellElement;
+//   let totaltext: HTMLTableCellElement = document.createElement("th");
+//   checkkouttotal2.appendChild(totaltext);
+//   totaltext.innerHTML = "total:";
 
-  for (let i: number = 0; i < astext.length; i++) {
-    let productt: HTMLTableCellElement = document.createElement("th");
-    titlecontainer.appendChild(productt);
-    productt.innerHTML = astext[i].name;
-    productt.className = "hej";
+//   for (let i: number = 0; i < astext.length; i++) {
+//     let productt: HTMLTableCellElement = document.createElement("th");
+//     titlecontainer.appendChild(productt);
+//     productt.innerHTML = astext[i].name;
+//     productt.className = "hej";
 
-    let amountt: HTMLTableCellElement = document.createElement("th");
-    amountcontainer.appendChild(amountt);
-    amountt.innerHTML = "x" + astext[i].amount;
-    amountt.className = "hej";
+//     let amountt: HTMLTableCellElement = document.createElement("th");
+//     amountcontainer.appendChild(amountt);
+//     amountt.innerHTML = "x" + astext[i].amount;
+//     amountt.className = "hej";
 
-    let amountqt: HTMLTableCellElement = document.createElement("th");
-    productquantity.appendChild(amountqt);
-    let amountplusbtn: HTMLButtonElement = document.createElement("button");
-    amountqt.appendChild(amountplusbtn);
-    amountqt.className = "hej";
+//     let amountqt: HTMLTableCellElement = document.createElement("th");
+//     productquantity.appendChild(amountqt);
+//     let amountplusbtn: HTMLButtonElement = document.createElement("button");
+//     amountqt.appendChild(amountplusbtn);
+//     amountqt.className = "hej";
 
-    let icon: HTMLSpanElement = document.createElement("i");
-    amountplusbtn.appendChild(icon);
+//     let icon: HTMLSpanElement = document.createElement("i");
+//     amountplusbtn.appendChild(icon);
 
-    icon.className = "fas fa-minus";
-    amountplusbtn.className = "plusbtn";
+//     icon.className = "fas fa-minus";
+//     amountplusbtn.className = "plusbtn";
 
-    let icon2: HTMLSpanElement = document.createElement("i");
-    icon2.className = "fas fa-plus";
+//     let icon2: HTMLSpanElement = document.createElement("i");
+//     icon2.className = "fas fa-plus";
 
-    let amountminusbtn: HTMLButtonElement = document.createElement("button");
-    amountqt.appendChild(amountminusbtn);
-    amountminusbtn.appendChild(icon2);
-    amountminusbtn.className = "minusbtn";
-  }
+//     let amountminusbtn: HTMLButtonElement = document.createElement("button");
+//     amountqt.appendChild(amountminusbtn);
+//     amountminusbtn.appendChild(icon2);
+//     amountminusbtn.className = "minusbtn";
+//   }
 
-  let addition: number = 0;
+//   let addition: number = 0;
 
-  for (let i = 0; i < astext.length; i++) {
-    addition += astext[i].price *= astext[i].amount;
-  }
+//   for (let i = 0; i < astext.length; i++) {
+//     addition += astext[i].price *= astext[i].amount;
+//   }
 
-  let totalprice2: HTMLTableCellElement = document.createElement("th");
-  checkkouttotal2.appendChild(totalprice2);
-  totalprice2.innerHTML = addition + "$";
-  totalprice2.id = "totalincenter";
-}
+//   let totalprice2: HTMLTableCellElement = document.createElement("th");
+//   checkkouttotal2.appendChild(totalprice2);
+//   totalprice2.innerHTML = addition + "$";
+//   totalprice2.id = "totalincenter";
+// }
